eed92407c57b2cbe3b2243b9539c27b89895616d
adylbek0408 <adylbek0408@icloud.com>
2025-02-26 12:30:48 +0600

    add static url

--- FILTER/settings.py
+++ FILTER/settings.py
@@ -110,6 +110,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_ROOT = '/app/static'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
